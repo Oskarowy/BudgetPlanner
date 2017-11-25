a579f2765ef72994684f275f3f263cfb40276d53
Oski <oskar.bialek.programista@gmail.com>
2017-11-25 23:49:15 +0100

    initiated abstract class of cash flows

---
+++ cashflow.cpp
@@ -0,0 +1,8 @@
+#include "cashflow.h"
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <conio.h>
+#include <algorithm>
+#include <windows.h>
---
+++ cashflow.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include "Markup.h"
+
+using namespace std;
+
+class CashFlow {
+
+protected:
+    int id, userID;
+    string date;
+    string item;
+    double amount;
+
+public:
+    CashFlow( int dID = 0, int dUserID = 0, string dDate = "2000-01-01", string dItem = "brak", double dAmount = 0.0 ) {
+        id = dID;
+        userID = dUserID;
+        this->date = dDate;
+        this->item = dItem;
+        this->amount = dAmount;
+    }
+
+    virtual int getID() = 0;
+    int getUserID();
+    string getDate();
+    string getItem();
+    double getAmount();
+
+    virtual void setID(int newID);
+    void setUserID(int loggedUserID);
+    void setDate();
+    void setItem();
+    void setAmount();
+    void setItem(string newValue);
+    void setAmount(double newValue);
+};
